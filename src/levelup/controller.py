2e6bc8b26a25298b8b075fea173c38e0ec3f20b0
IsabelLamptey <isabel2lisa@gmail.com>
2023-09-20 19:03:51 +0000

    modified controller file

--- src/levelup/controller.py
+++ src/levelup/controller.py
@@ -2,12 +2,16 @@ import logging
 from dataclasses import dataclass
 from enum import Enum
 
-#TODO: ADD THINGS YOU NEED FOR STATUS
+@dataclass
 class GameStatus:
     character_name: str = "Character"
     move_count: int = 0
     running: bool = False
+    current_position : tuple = (-100,-100)
 
+    def set_character_position(self, xycoordinates: tuple) -> None:
+        print("Set character position state for testing")
+        # TODO :Implement this
 class Direction(Enum):
     NORTH = "n"
     SOUTH = "s"
