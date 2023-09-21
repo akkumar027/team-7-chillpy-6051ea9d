70e7681c7412f3808480a9ea180f9c3c7cf6da11
IsabelLamptey <isabel2lisa@gmail.com>
2023-09-21 20:04:22 +0000

    Fix position

--- tests/test_position.py
+++ tests/test_position.py
@@ -6,11 +6,7 @@ class TestPositionInitWithCoordinates(TestCase):
     
     def test_init(self):
        CurrentPosition = (-1,-1)
-       testobj = Position(CurrentPosition["0"],CurrentPosition[1])
+
+       testobj = Position(CurrentPosition[0],CurrentPosition[1])
        self.assertEqual(CurrentPosition, testobj.coordinates)
-      # self.assertFalse(CurrentPosition,testobj.coordinates)
-      ''''``` def test_init(self1):
-       CurrentPosition = (-1,-1)
-       testobj2 = Position(CurrentPosition[11],CurrentPosition[1])
-       self1.assertEqual(CurrentPosition, testobj2.coordinates)   
-''''
+
