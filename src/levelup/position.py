78be3457deb2514b697ad203e607694878d90f87
Micheal <t.michael@theherrings.org>
2023-09-21 19:18:45 +0000

    failing test?

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -12,7 +12,9 @@ class Position:
        
        PointCoordinates = Position( xCoordinates, yCoordinate)
        '''
-
+    def __init__(self):
+        # do nothing
+        self.coordinates = (-1, -1)
 
     def __init__(self, xCoordinate,yCoordinate):
         self.coordinates = (xCoordinate,yCoordinate)
