b74f6b707f5971bc38b6e2b931829a781ec2574f
IsabelLamptey <isabel2lisa@gmail.com>
2023-09-21 18:52:09 +0000

    corrected class

--- tests/test_position.py
+++ tests/test_position.py
@@ -4,6 +4,6 @@ from levelup.position import Position
 class TestPositionInitWithCoordinates(TestCase):
     def test_init(self):
        CurrentPosition = (-1,-1)
-       testobj = position(CurrentPosition[0],CurrentPosition[1])
+       testobj = Position(CurrentPosition[0],CurrentPosition[1])
        self.assertEqual(CurrentPosition, testobj.coordinates)
        
