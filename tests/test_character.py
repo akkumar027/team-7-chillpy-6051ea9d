c3a357e39910f177ab2103990c3a7cbccd7eccc6
Abhishek Kumar <akkumar027.us@gmail.com>
2023-09-21 19:50:58 +0000

    remove syntax map

--- tests/test_character.py
+++ tests/test_character.py
@@ -6,12 +6,11 @@ class TestCharacter(TestCase):
     def setUp(self):
         ARBITRARY_NAME = "Bob"
         self.testobj = Character(ARBITRARY_NAME)
-        self.map
         
     def test_character_not_empty(self):
         self.assertGreater(len(self.testobj.get_name()), 0)
 
     def test_character_same_name(self):
-        ARBITRARY_NAME = "Bob"
-        self.assertEqual(self.testobj.get_name(), ARBITRARY_NAME)
+        EXPECTED_NAME = "Bob"
+        self.assertEqual(self.testobj.get_name(), EXPECTED_NAME)
 
